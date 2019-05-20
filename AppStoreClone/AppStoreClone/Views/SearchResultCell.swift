de0d84eac5efdc72c3026fdf9d231182e4ad193f
Medi Assumani <mediassumani49@gmail.com>
2019-05-20 11:28:32 -0700

    [Update] custom cell with app informations

--- AppStoreClone/AppStoreClone/Views/SearchResultCell.swift
+++ AppStoreClone/AppStoreClone/Views/SearchResultCell.swift
@@ -16,6 +16,7 @@ class SearchResultCell: UICollectionViewCell {
         
         let iv = UIImageView()
         iv.backgroundColor = .red
+        iv.widthAnchor.constraint(equalToConstant: 64).isActive = true
         return iv
     }()
     
@@ -23,16 +24,35 @@ class SearchResultCell: UICollectionViewCell {
        
         let label = UILabel()
         label.text = "App Name"
-        
         return label
     }()
     
-    let getButton: UIButton
+    let getButton: UIButton = {
+        
+        let button = UIButton(type: .system)
+        button.setTitle("GET", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        button.backgroundColor = UIColor(white: 0.95, alpha: 1)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 14)
+        button.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        return button
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         backgroundColor = .green
+        
+        let stackView = UIStackView(arrangedSubviews: [appThumbnailView, appTitleLabel, getButton])
+        stackView.spacing = 12
+        
+        addSubview(stackView)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.safeAreaLayoutGuide.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        stackView.safeAreaLayoutGuide.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        stackView.safeAreaLayoutGuide.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        stackView.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
     }
     
     required init?(coder aDecoder: NSCoder) {
