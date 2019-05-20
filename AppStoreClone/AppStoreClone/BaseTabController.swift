519c2527e21309b501bfbd66bf7c6ed23f36fe8c
Medi Assumani <mediassumani49@gmail.com>
2019-05-20 09:16:46 -0700

    [Add] initial settings

---
+++ AppStoreClone/AppStoreClone/BaseTabController.swift
@@ -0,0 +1,19 @@
+//
+//  BaseTabController.swift
+//  AppStoreClone
+//
+//  Created by Medi Assumani on 5/20/19.
+//  Copyright © 2019 Medi Assumani. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BaseTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .yellow
+    }
+}
