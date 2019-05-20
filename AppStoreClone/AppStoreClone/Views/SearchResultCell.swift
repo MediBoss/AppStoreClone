73f503cbb2ae2542d7d864bb630ebe0cd74a38a0
Medi Assumani <mediassumani49@gmail.com>
2019-05-20 11:39:57 -0700

    [Update] completed lesson 4

--- AppStoreClone/AppStoreClone/Views/SearchResultCell.swift
+++ AppStoreClone/AppStoreClone/Views/SearchResultCell.swift
@@ -17,6 +17,8 @@ class SearchResultCell: UICollectionViewCell {
         let iv = UIImageView()
         iv.backgroundColor = .red
         iv.widthAnchor.constraint(equalToConstant: 64).isActive = true
+        iv.heightAnchor.constraint(equalToConstant: 64).isActive = true
+        iv.layer.cornerRadius = 12
         return iv
     }()
     
@@ -27,14 +29,33 @@ class SearchResultCell: UICollectionViewCell {
         return label
     }()
     
+    let categoryLabel: UILabel = {
+        
+        let label = UILabel()
+        label.text = "Photos & Video"
+        return label
+    }()
+
+    
+    let ratingsLabel: UILabel = {
+        
+        let label = UILabel()
+        label.text = "9.67M"
+        return label
+    }()
+
+    
     let getButton: UIButton = {
         
         let button = UIButton(type: .system)
+        
         button.setTitle("GET", for: .normal)
         button.setTitleColor(.blue, for: .normal)
         button.backgroundColor = UIColor(white: 0.95, alpha: 1)
         button.titleLabel?.font = .boldSystemFont(ofSize: 14)
+        button.layer.cornerRadius = 15
         button.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        
         return button
     }()
     
@@ -43,16 +64,20 @@ class SearchResultCell: UICollectionViewCell {
         
         backgroundColor = .green
         
-        let stackView = UIStackView(arrangedSubviews: [appThumbnailView, appTitleLabel, getButton])
+        let labelsStackView = UIStackView(arrangedSubviews: [appTitleLabel, categoryLabel, ratingsLabel])
+        labelsStackView.axis = .vertical
+        
+        let stackView = UIStackView(arrangedSubviews: [appThumbnailView, labelsStackView, getButton])
         stackView.spacing = 12
+        stackView.alignment = .center
         
         addSubview(stackView)
         
         stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.safeAreaLayoutGuide.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-        stackView.safeAreaLayoutGuide.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
-        stackView.safeAreaLayoutGuide.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
-        stackView.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        stackView.safeAreaLayoutGuide.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor).isActive = true
+        stackView.safeAreaLayoutGuide.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 5).isActive = true
+        stackView.safeAreaLayoutGuide.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -5).isActive = true
+        stackView.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor).isActive = true
     }
     
     required init?(coder aDecoder: NSCoder) {
